bb90ba41a511e5ac5c8159c11613ad15bd4a32e5
d.putans <dainis.putans@baptisthealth.net>
2022-06-08 19:13:58 +0300

    Initial Commit

---
+++ warmUp-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  warmUp-1
+//
+//  Created by d.putans on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

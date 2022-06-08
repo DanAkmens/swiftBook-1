6d2bb4783c277c71f137f074afe006dee3d3c346
d.putans <d.putans@accenture.com>
2022-06-09 00:36:35 +0300

    Create hide/show text logic.

--- warmUp-1/ViewController.swift
+++ warmUp-1/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var warmUpLabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        warmUpLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if warmUpLabel.isHidden {
+            warmUpLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            warmUpLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
